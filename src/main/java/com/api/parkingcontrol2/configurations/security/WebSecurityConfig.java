0d036f6a287eb489323b2a9d68a932237260254f
Sandro Livino <sandro.livino@gmail.com>
2023-02-27 19:52:29 -0300

    Customização para autenticação em memória + senha com password encoder

--- src/main/java/com/api/parkingcontrol2/configurations/security/WebSecurityConfig.java
+++ src/main/java/com/api/parkingcontrol2/configurations/security/WebSecurityConfig.java
@@ -1,8 +1,12 @@
 package com.api.parkingcontrol2.configurations.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -15,5 +19,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("sandro")
+                .password(this.passwordEncoder().encode("607080"))
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
