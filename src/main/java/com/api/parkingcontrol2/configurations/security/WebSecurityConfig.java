1f08a6146976a703a59cdbb3979d71cf5f6018f1
Sandro Livino <sandro.livino@gmail.com>
2023-02-27 15:52:51 -0300

    Implementando a classe WebSecurityConfig para requisitar autenticação nos endpoints e bypassar crsf

---
+++ src/main/java/com/api/parkingcontrol2/configurations/security/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.api.parkingcontrol2.configurations.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .httpBasic()
+                .and()
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+    }
+}
