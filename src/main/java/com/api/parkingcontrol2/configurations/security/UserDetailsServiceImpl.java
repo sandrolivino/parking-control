b905fa87770edac3f23384c5d7ad46aa62d1a913
Sandro Livino <sandro.livino@gmail.com>
2023-03-02 12:40:30 -0300

    Controle de acessos com definição de roles

--- src/main/java/com/api/parkingcontrol2/configurations/security/UserDetailsServiceImpl.java
+++ src/main/java/com/api/parkingcontrol2/configurations/security/UserDetailsServiceImpl.java
@@ -3,12 +3,16 @@ package com.api.parkingcontrol2.configurations.security;
 import com.api.parkingcontrol2.models.UserModel;
 import com.api.parkingcontrol2.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
+@Transactional
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     UserRepository userRepository;
@@ -16,6 +20,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserModel userModel = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
-        return  null;
+        return new User(userModel.getUsername(), userModel.getPassword(), true, true, true,true, userModel.getAuthorities());
     }
 }
---
+++ src/main/java/com/api/parkingcontrol2/enums/RoleName.java
@@ -0,0 +1,6 @@
+package com.api.parkingcontrol2.enums;
+
+public enum RoleName {
+    ROLE_ADMIN,
+    ROLE_USER;
+}
