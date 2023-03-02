90d7e52b859589f17b342b89f63c01a1f2854a72
Sandro Livino <sandro.livino@gmail.com>
2023-03-02 13:55:42 -0300

    Atualização: Configurando o Spring Security com componentes (sem o WebSecurityConfigurerAdapter)

---
+++ src/main/java/com/api/parkingcontrol2/configurations/security/WebSecurityConfigV2.java
@@ -0,0 +1,34 @@
+package com.api.parkingcontrol2.configurations.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfigV2 {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        // Configurações (idênticas ao método antigo da classe WebSecurityConfig)
+        http
+                .httpBasic()
+                .and()
+                .authorizeHttpRequests()
+                .antMatchers(HttpMethod.GET, "parking-spot/**").permitAll()
+                .antMatchers(HttpMethod.POST, "parking-spot/").hasRole("USER")
+                .antMatchers(HttpMethod.DELETE, "parking-spot/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
